11c68e73b88f0a4e6b2e051e93afae0b6aa37cbc
Alexis Garcia <garciaalexis942@gmail.com>
2026-05-05 12:27:23 -0500

    Solucion Error CodeSmell Angular

--- admin_clinica/src/app/core/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ admin_clinica/src/app/core/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { routes } from 'src/app/shared/routes/routes';
 import {
   ApexAxisChartSeries,
